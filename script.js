72691e7914ee1bb0f7d19d8560b2de8f917abb83
andrey post <postoliuk.av@gmail.com>
2021-06-09 11:12:07 +0300

    minus learning pieiod from ex

--- script.js
+++ script.js
@@ -1,54 +1,64 @@
 const widthGreenLine = (elems) => {
-	for (let elem of elems) {
-		elem.querySelector('.growLine').style.width = elem.previousElementSibling.querySelector('.rateLine').innerHTML + '%'
-	}
-}
-widthGreenLine(document.querySelectorAll('.baseLine'))
-document.querySelector('.milleseconds').innerHTML = parseInt((Date.now() - new Date(2017, 1, 11)) / 86400000)
+  for (let elem of elems) {
+    elem.querySelector('.growLine').style.width =
+      elem.previousElementSibling.querySelector('.rateLine').innerHTML + '%';
+  }
+};
+widthGreenLine(document.querySelectorAll('.baseLine'));
+document.querySelector('.milleseconds').innerHTML = parseInt(
+  (Date.now() - new Date(2018, 0, 28)) / 86400000
+);
 // document.querySelector('.milleseconds').innerHTML = Date.now() - new Date(2017)
-document.querySelector('.year').innerHTML = new Date().getFullYear()
+document.querySelector('.year').innerHTML = new Date().getFullYear();
 // document.querySelector('.expYear').innerHTML = new Date().getFullYear() - 2017 + '+'
 const Slider = (options) => {
-	let elems = options.elems, prev = options.prev, next = options.next, count = 0
-	const slide = () => {
-		for (let elem of elems) {
-			elem.classList.add('hidden')
-		}
-		elems[count].classList.remove('hidden')
-	}
-	slide()
-	prev.onclick = () => {
-		count <= 0 ? count = elems.length - 1 : count--
-		slide()
-	}
-	next.onclick = () => {
-		count >= elems.length - 1 ? count = 0 : count++
-		slide()
-	}
-}
+  let elems = options.elems,
+    prev = options.prev,
+    next = options.next,
+    count = 0;
+  const slide = () => {
+    for (let elem of elems) {
+      elem.classList.add('hidden');
+    }
+    elems[count].classList.remove('hidden');
+  };
+  slide();
+  prev.onclick = () => {
+    count <= 0 ? (count = elems.length - 1) : count--;
+    slide();
+  };
+  next.onclick = () => {
+    count >= elems.length - 1 ? (count = 0) : count++;
+    slide();
+  };
+};
 Slider({
-	elems: document.querySelectorAll('.slider > div'),
-	prev: document.querySelector('.prev'),
-	next: document.querySelector('.next')
-})
-let arrow = document.getElementById('arrowUp')
+  elems: document.querySelectorAll('.slider > div'),
+  prev: document.querySelector('.prev'),
+  next: document.querySelector('.next'),
+});
+let arrow = document.getElementById('arrowUp');
 window.addEventListener('scroll', () => {
-	arrow.hidden = document.documentElement.clientHeight / 2 > document.documentElement.scrollTop
-})
+  arrow.hidden =
+    document.documentElement.clientHeight / 2 >
+    document.documentElement.scrollTop;
+});
 document.addEventListener('click', (e) => {
-	if (e.target.dataset.section) {
-		e.preventDefault()
-		window.scrollBy({
-			top: document.querySelector(`.${e.target.dataset.section}`).getBoundingClientRect().top,
-			behavior: 'smooth'
-		})
-	} else if (e.target.closest('#arrowUp')) {
-		window.scrollBy({
-			top: -window.scrollY,
-			behavior: 'smooth'
-		})
-	}
-})
+  if (e.target.dataset.section) {
+    e.preventDefault();
+    window.scrollBy({
+      top: document
+        .querySelector(`.${e.target.dataset.section}`)
+        .getBoundingClientRect().top,
+      behavior: 'smooth',
+    });
+  } else if (e.target.closest('#arrowUp')) {
+    window.scrollBy({
+      top: -window.scrollY,
+      behavior: 'smooth',
+    });
+  }
+});
 /*
 arrowUp.onclick = function (e) {
 	let start = Date.now()
@@ -82,4 +92,3 @@ arrowUp.onclick = function (e) {
 // 		}
 // 	}
 // }
-
